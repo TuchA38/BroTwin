7d030318f1ff2d1c1290ae04d30e4b1b2e6ba9c3
Przemo1211 <121610311+Przemo1211@users.noreply.github.com>
2023-09-20 10:23:04 +0200

    Add files via upload

---
+++ Muzyka/Oceania/js/music-list.js
@@ -0,0 +1,25 @@
+let allMusic = [{
+        name: "Pakiet Oceania - Raj pingwinów",
+        artist: "Oceania Pack",
+        img: "music",
+        src: "music-108"
+    },
+    {
+        name: "Pakiet Oceania - Krocz z wdziękiem kuoki",
+        artist: "Oceania Pack",
+        img: "music",
+        src: "music-109"
+    },
+    {
+        name: "Pakiet Oceania - Kiwi na luzaku",
+        artist: "Oceania Pack",
+        img: "music",
+        src: "music-110"
+    },
+    {
+        name: "Pakiet Oceania - Latające rudawki",
+        artist: "Oceania Pack",
+        img: "music",
+        src: "music-111"
+    }
+];
